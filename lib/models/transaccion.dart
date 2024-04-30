3022da397528678ae3ae7e260db99c1927491b6c
julio <juliogonzales1302@gmail.com>
2024-04-30 17:57:00 -0400

    transaccion

---
+++ lib/models/transaccion.dart
@@ -0,0 +1,115 @@
+import 'dart:convert';
+
+Transaccion transaccionFromJson(String str) => Transaccion.fromJson(json.decode(str));
+
+String transaccionToJson(Transaccion data) => json.encode(data.toJson());
+
+class Transaccion {
+    int? id;
+    int? monto;
+    DateTime? fecha;
+    String? estado;
+    String? partida;
+    String? ronda;
+    Deudor? deudor;
+    Receptor? receptor;
+    List<Transaccion> toList = [];
+
+    Transaccion({
+        this.id,
+        this.monto,
+        this.fecha,
+        this.estado,
+        this.partida,
+        this.ronda,
+        this.deudor,
+        this.receptor,
+    });
+
+    factory Transaccion.fromJson(Map<String, dynamic> json) => Transaccion(
+        id: json["id"],
+        monto: json["monto"],
+        fecha: DateTime.parse(json["fecha"]),
+        estado: json["estado"],
+        partida: json["partida"],
+        ronda: json["ronda"],
+        deudor: Deudor.fromJson(json["deudor"]),
+        receptor: Receptor.fromJson(json["receptor"]),
+    );
+
+    Transaccion.fromJsonList(List<dynamic> jsonList){
+      if (jsonList == null) return;
+        jsonList.forEach((item) {
+          
+          Transaccion transaccion = Transaccion.fromJson(item);
+          //print("invitacion::: ${invitacion.jugadorNombre}");
+          toList.add(transaccion);
+      });
+    }
+
+    Map<String, dynamic> toJson() => {
+        "id": id,
+        "monto": monto,
+        "fecha": fecha?.toIso8601String(),
+        "estado": estado,
+        "partida": partida,
+        "ronda": ronda,
+        "deudor": deudor?.toJson(),
+        "receptor": receptor?.toJson(),
+    };
+}
+
+class Deudor {
+    int id;
+    String nombre;
+
+    Deudor({
+        required this.id,
+        required this.nombre,
+    });
+
+    factory Deudor.fromJson(Map<String, dynamic> json) => Deudor(
+        id: json["id"],
+        nombre: json["nombre"],
+    );
+
+    Map<String, dynamic> toJson() => {
+        "id": id,
+        "nombre": nombre,
+    };
+}
+
+class Receptor {
+    int id;
+    String nombre;
+    String imagen;
+
+    Receptor({
+        required this.id,
+        required this.nombre,
+        required this.imagen,
+    });
+
+    factory Receptor.fromJson(Map<String, dynamic> json) => Receptor(
+        id: json["id"],
+        nombre: json["nombre"],
+        imagen: json["imagen"],
+    );
+
+    Map<String, dynamic> toJson() => {
+        "id": id,
+        "nombre": nombre,
+        "imagen": imagen,
+    };
+}
+
+/*
+Transaccion.fromJsonList(List<dynamic> jsonList){
+      if (jsonList == null) return;
+        jsonList.forEach((item) {
+          
+          Transaccion transaccion = Transaccion.fromJson(item);
+          //print("invitacion::: ${invitacion.jugadorNombre}");
+          toList.add(transaccion);
+      });
+    } */
